318c6d21d687f61a0f303d879d809886cec82e79
Malthe Borch <mborch@gmail.com>
2008-08-28 07:29:17 +0000

    Version bump.
    svn path=/z3c.formwidget.query/trunk/; revision=90514

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='z3c.formwidget.query',
-      version='0.2',
+      version='0.4dev',
       author = "Zope Community",
       author_email = "zope3-dev@zope.org",
       description = "A source query widget for z3c.form.",
