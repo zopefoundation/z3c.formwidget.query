3038308aa643465b82987797d2573c481191ccb9
Vincent Fretin <vincent.fretin@gmail.com>
2014-04-08 14:45:21 +0000

    remove trailing whitespaces
    svn path=/z3c.formwidget.query/trunk/; revision=130404

--- src/z3c/formwidget/query/widget.py
+++ src/z3c/formwidget/query/widget.py
@@ -27,27 +27,27 @@ except ImportError:
 
 
 class SourceTerms(z3c.form.term.Terms):
-    
+
     def __init__(self, context, request, form, field, widget, source):
         self.context = context
         self.request = request
         self.form = form
         self.field = field
         self.widget = widget
-        
+
         self.terms = source
 
 class QueryTerms(z3c.form.term.Terms):
-    
+
     def __init__(self, context, request, form, field, widget, terms):
         self.context = context
         self.request = request
         self.form = form
         self.field = field
         self.widget = widget
-        
+
         self.terms = SimpleVocabulary(terms)
-            
+
 class QuerySubForm(z3c.form.form.Form):
     zope.interface.implements(z3c.form.interfaces.ISubForm)
     css_class = 'querySelectSearch'
@@ -61,7 +61,7 @@ class QuerySubForm(z3c.form.form.Form):
         super(QuerySubForm, self).__init__(context, request)
 
         if prefix is not None:
-            self.prefix = prefix    
+            self.prefix = prefix
 
     @z3c.form.button.buttonAndHandler(_(u"Search"))
     def search(self, action):
@@ -74,7 +74,7 @@ class QueryContext(object):
 
 class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
     """Query source widget that allows single selection."""
-    
+
     _radio = True
     _queryform = None
     _resultsform = None
@@ -100,7 +100,7 @@ class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
         return self._bound_source
 
     def update(self):
-        
+
         # Allow the source to provide terms until we have more specific ones
         # from the query. Things do not go well if self.terms is None
 
@@ -108,10 +108,10 @@ class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
         source = self.bound_source
 
         self.terms = SourceTerms(self.context, self.request, self.form, self.field, self, source)
-        
+
         # If we have values in the request, use these to get the terms.
         # Otherwise, take the value from the current saved value.
-        
+
         terms = []
 
         request_values = z3c.form.interfaces.NOVALUE
@@ -133,15 +133,15 @@ class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
                         raise
 
         elif not self.ignoreContext:
-            
+
             selection = zope.component.getMultiAdapter(
                 (self.context, self.field), z3c.form.interfaces.IDataManager).query()
-            
+
             if selection is z3c.form.interfaces.NOVALUE:
                 selection = []
             elif not isinstance(selection, (tuple, set, list)):
                 selection = [selection]
-            
+
             for value in selection:
                 if not value:
                     continue
@@ -171,7 +171,7 @@ class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
                     try:
                         terms.append(source.getTerm(value))
                     except LookupError:
-                        # Term no longer available                             
+                        # Term no longer available
                         if not self.ignoreMissing:
                             raise
 
@@ -195,7 +195,7 @@ class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
                 for term in query_terms:
                     if term.token not in tokens:
                         terms.append(term)
-        
+
         # set terms
         self.terms = QueryTerms(self.context, self.request, self.form, self.field, self, terms)
 
@@ -214,7 +214,7 @@ class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
 
     def extract(self, default=z3c.form.interfaces.NOVALUE):
         return self.extractQueryWidget(default)
-        
+
     def render(self):
         subform = self.subform
         return "\n".join((subform.render(), self.renderQueryWidget()))
@@ -230,14 +230,14 @@ class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
 
     def renderQueryWidget(self):
         return z3c.form.browser.radio.RadioWidget.render(self)
-        
+
     def extractQueryWidget(self, default=z3c.form.interfaces.NOVALUE):
         return z3c.form.browser.radio.RadioWidget.extract(self, default)
 
 class QuerySourceCheckboxWidget(
     QuerySourceRadioWidget, z3c.form.browser.checkbox.CheckBoxWidget):
     """Query source widget that allows multiple selections."""
-    
+
     zope.interface.implementsOnly(z3c.form.interfaces.ICheckBoxWidget)
 
     _radio = False
@@ -251,7 +251,7 @@ class QuerySourceCheckboxWidget(
 
     def renderQueryWidget(self):
         return z3c.form.browser.checkbox.CheckBoxWidget.render(self)
-    
+
     def extractQueryWidget(self, default=z3c.form.interfaces.NOVALUE):
         return z3c.form.browser.checkbox.CheckBoxWidget.extract(self, default)
 
