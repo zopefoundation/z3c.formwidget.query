c2430634c9f2e5d3e9c6fa4a59a95a843c1c78d0
Maurits van Rees <maurits@vanrees.org>
2015-09-09 00:25:59 +0200

    pep8 in setup.py, mostly reindentation.

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(name='z3c.formwidget.query',
       author_email = "zope3-dev@zope.org",
       description = "A source query widget for z3c.form.",
       long_description=(
-          open('README.rst').read()
-          + '\n\n' +
+          open('README.rst').read() + '\n\n' +
           open('CHANGES.rst').read()
           ),
       license = "ZPL 2.1",
@@ -17,25 +16,31 @@ setup(name='z3c.formwidget.query',
       zip_safe=False,
       packages=find_packages('src'),
       include_package_data=True,
-      package_dir = {'':'src'},
+      package_dir = {'': 'src'},
       namespace_packages=['z3c', 'z3c.formwidget'],
-      extras_require = dict(test=['zope.testing',
-                                  'z3c.form [test]'
-                                  ]),
-      install_requires = ['setuptools',
-                          'z3c.form >= 2.6.0',
-                          'zope.interface',
-                          'zope.schema',
-                          'zope.component',
-                          'zope.i18nmessageid',
-                          ],
-      classifiers = ['Development Status :: 4 - Beta',
-                     'Environment :: Web Environment',
-                     'Framework :: Zope3',
-                     'Intended Audience :: Developers',
-                     'License :: OSI Approved :: Zope Public License',
-                     'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-                     'Programming Language :: Python',
-                     'Topic :: Software Development :: Libraries :: Python Modules',
-                     ],
+      extras_require = dict(
+          test=[
+              'zope.testing',
+              'z3c.form [test]'
+              ]),
+      install_requires = [
+          'setuptools',
+          'z3c.form >= 2.6.0',
+          'zope.interface',
+          'zope.schema',
+          'zope.component',
+          'zope.i18nmessageid',
+          ],
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Framework :: Zope3',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
       )
