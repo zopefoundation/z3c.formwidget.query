90d78e7c443ab60339e7be530c851953eaae878a
Maurits van Rees <maurits@vanrees.org>
2023-04-27 00:42:00 +0200

    Preparing release 2.0.0
    [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='z3c.formwidget.query',
-    version='2.0.dev0',
+    version='2.0.0',
     author="Zope Community",
     author_email="zope-dev@zope.dev",
     description="A source query widget for z3c.form.",
